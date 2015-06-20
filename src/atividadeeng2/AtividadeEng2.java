e3fa2f2106218886c256497b6880a2e90c2e0c6f
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 16:05:12 -0300

    versao 1.0

---
+++ src/atividadeeng2/AtividadeEng2.java
@@ -0,0 +1,36 @@
+
+package atividadeeng2;
+
+/**
+ *
+ * @author NICOLAS
+ */
+public class AtividadeEng2 {
+
+    private int A1;
+    
+
+    public int getA1() {
+        return A1;
+    }
+    private float A2;
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA2(float A2) {
+        this.A2 = A2;
+    }
+
+
+    public void setA1(int A1) {
+        this.A1 = A1;
+    }
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
